d15dbec11875ae1777b2f39b3bd37df2a7987b20
Rishabh Kumar <rishabh5799kumar@gmail.com>
2026-03-15 19:11:15 +0530

    add: users model

---
+++ users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    website = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
